b811d1741f998114c284f9d4e674be3d5f4c931d
Guide42-ho <mophone563@gmail.com>
2024-09-28 23:09:16 +0700

    Add files via upload
    UpdateDatastructure

--- Datastructure/GUICollection/Form1.Designer.cs
+++ Datastructure/GUICollection/Form1.Designer.cs
@@ -28,87 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.AddTextBox = new System.Windows.Forms.TextBox();
+            this.GetIndextextBox = new System.Windows.Forms.TextBox();
+            this.ShowTextBox = new System.Windows.Forms.TextBox();
+            this.QuantityLabel = new System.Windows.Forms.Label();
+            this.GetIndex = new System.Windows.Forms.Button();
+            this.Set = new System.Windows.Forms.Button();
+            this.NameAllListBox = new System.Windows.Forms.ListBox();
+            this.ShowAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Add
             // 
-            this.button1.Location = new System.Drawing.Point(332, 102);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "เพิ่มชื่อ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Add.Location = new System.Drawing.Point(277, 54);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "เพิ่มชื่อ";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
-            // button2
+            // Search
             // 
-            this.button2.Location = new System.Drawing.Point(332, 185);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "ค้นหาชื่อ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Search.Location = new System.Drawing.Point(375, 54);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 2;
+            this.Search.Text = "ค้นหาชื่อ";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
             // 
-            // button3
+            // Delete
             // 
-            this.button3.Location = new System.Drawing.Point(332, 257);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "ลบชื่อ";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.Delete.Location = new System.Drawing.Point(478, 55);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "ลบชื่อ";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
             // 
-            // textBox1
+            // AddTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(189, 102);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 4;
+            this.AddTextBox.Location = new System.Drawing.Point(35, 55);
+            this.AddTextBox.Name = "AddTextBox";
+            this.AddTextBox.Size = new System.Drawing.Size(219, 22);
+            this.AddTextBox.TabIndex = 4;
             // 
-            // textBox2
+            // GetIndextextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(189, 186);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 5;
+            this.GetIndextextBox.Location = new System.Drawing.Point(35, 103);
+            this.GetIndextextBox.Name = "GetIndextextBox";
+            this.GetIndextextBox.Size = new System.Drawing.Size(100, 22);
+            this.GetIndextextBox.TabIndex = 5;
             // 
-            // textBox3
+            // ShowTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(189, 257);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 6;
+            this.ShowTextBox.Location = new System.Drawing.Point(317, 103);
+            this.ShowTextBox.Name = "ShowTextBox";
+            this.ShowTextBox.Size = new System.Drawing.Size(147, 22);
+            this.ShowTextBox.TabIndex = 6;
             // 
-            // label1
+            // QuantityLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(669, 423);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 16);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "จำนวน 0 คน";
+            this.QuantityLabel.AutoSize = true;
+            this.QuantityLabel.Location = new System.Drawing.Point(669, 423);
+            this.QuantityLabel.Name = "QuantityLabel";
+            this.QuantityLabel.Size = new System.Drawing.Size(65, 16);
+            this.QuantityLabel.TabIndex = 7;
+            this.QuantityLabel.Text = "จำนวน 0 คน";
+            // 
+            // GetIndex
+            // 
+            this.GetIndex.Location = new System.Drawing.Point(163, 103);
+            this.GetIndex.Name = "GetIndex";
+            this.GetIndex.Size = new System.Drawing.Size(120, 23);
+            this.GetIndex.TabIndex = 8;
+            this.GetIndex.Text = "ค้นหาชื่อจากลำดับที่";
+            this.GetIndex.UseVisualStyleBackColor = true;
+            this.GetIndex.Click += new System.EventHandler(this.GetIndex_Click);
+            // 
+            // Set
+            // 
+            this.Set.Location = new System.Drawing.Point(478, 103);
+            this.Set.Name = "Set";
+            this.Set.Size = new System.Drawing.Size(75, 23);
+            this.Set.TabIndex = 9;
+            this.Set.Text = "แก้ไข";
+            this.Set.UseVisualStyleBackColor = true;
+            this.Set.Click += new System.EventHandler(this.Set_Click);
+            // 
+            // NameAllListBox
+            // 
+            this.NameAllListBox.FormattingEnabled = true;
+            this.NameAllListBox.ItemHeight = 16;
+            this.NameAllListBox.Location = new System.Drawing.Point(82, 216);
+            this.NameAllListBox.Name = "NameAllListBox";
+            this.NameAllListBox.Size = new System.Drawing.Size(201, 148);
+            this.NameAllListBox.TabIndex = 10;
+            // 
+            // ShowAll
+            // 
+            this.ShowAll.Location = new System.Drawing.Point(375, 254);
+            this.ShowAll.Name = "ShowAll";
+            this.ShowAll.Size = new System.Drawing.Size(109, 23);
+            this.ShowAll.TabIndex = 11;
+            this.ShowAll.Text = "แสดงรายชื่อทั้งหมด";
+            this.ShowAll.UseVisualStyleBackColor = true;
+            this.ShowAll.Click += new System.EventHandler(this.ShowAll_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ShowAll);
+            this.Controls.Add(this.NameAllListBox);
+            this.Controls.Add(this.Set);
+            this.Controls.Add(this.GetIndex);
+            this.Controls.Add(this.QuantityLabel);
+            this.Controls.Add(this.ShowTextBox);
+            this.Controls.Add(this.GetIndextextBox);
+            this.Controls.Add(this.AddTextBox);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Add);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "รายชื่อ";
             this.ResumeLayout(false);
@@ -117,13 +165,17 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.TextBox AddTextBox;
+        private System.Windows.Forms.TextBox GetIndextextBox;
+        private System.Windows.Forms.TextBox ShowTextBox;
+        private System.Windows.Forms.Label QuantityLabel;
+        private System.Windows.Forms.Button GetIndex;
+        private System.Windows.Forms.Button Set;
+        private System.Windows.Forms.ListBox NameAllListBox;
+        private System.Windows.Forms.Button ShowAll;
     }
 }
 
--- Datastructure/GUICollection/Form1.cs
+++ Datastructure/GUICollection/Form1.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Collections;
 using Sets;
+using Lists;
+using System.Xml.Linq;
 
 namespace GUICollection
 {
@@ -20,32 +22,40 @@ namespace GUICollection
             InitializeComponent();
         }
         
-        Set C = new ArraySet(1);
+        List C = new ArrayList(1);
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Add_Click(object sender, EventArgs e)
         {
-            C.add(textBox1.Text);
-            textBox1.Clear();
-            Updatelabel1();
+            if (!AddTextBox.Text.Equals("")) 
+            {
+                C.add(AddTextBox.Text);
+                AddTextBox.Clear();
+                UpdateQuantityLabel();
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Search_Click(object sender, EventArgs e)
         {
-            C.contains(textBox2.Text);
-            message(textBox2.Text);
-            textBox2.Clear();
-        }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            C.remove(textBox3.Text);
-            textBox3.Clear();
-            Updatelabel1();
+            if (!AddTextBox.Text.Equals("")) 
+            {
+                C.contains(AddTextBox.Text);
+                message(AddTextBox.Text);
+                AddTextBox.Clear();
+            }
             
         }
-        private void Updatelabel1()
+
+        private void Delete_Click(object sender, EventArgs e)
         {
-            label1.Text = "จำนวน"+C.size()+"คน";
+            C.remove(AddTextBox.Text);
+            AddTextBox.Clear();
+            UpdateQuantityLabel();
+            
+        }
+        private void UpdateQuantityLabel()
+        {
+            QuantityLabel.Text = "จำนวน "+C.size()+" คน";
         }
         private void message(object e)
         {
@@ -57,6 +67,60 @@ namespace GUICollection
             else
             {
                 MessageBox.Show("ไม่มีข้อมูล : " + e);
+            }
+        }
+        private void GetIndex_Click(object sender, EventArgs e)
+        {
+            int index = Index();
+            if (C.contains(index) || index == -2) 
+            {
+                MessageBox.Show("ต้องเป็นตัวเลขเท่านั้น");
+            }
+            if (index > C.size() || C.isEmpty()) 
+            {
+                MessageBox.Show("ไม่มีข้อมูลในตำแหน่งที่ : "+ ++index);
+            }
+            else
+            {
+                ShowTextBox.Text = (String)C.get(index);
+            }
+        }
+        private void Set_Click(object sender, EventArgs e)
+        {
+            int index = Index();
+            if (C.size() < index)
+            {
+                MessageBox.Show("ไม่มีข้อมูลที่จะแก้ไข");
+            }
+            if (index != -2 & !ShowTextBox.Text.Equals("")) 
+            {
+            C.set(index, ShowTextBox.Text);
+            ShowTextBox.Clear();GetIndextextBox.Clear();
+            }
+            else 
+            {
+                MessageBox.Show("ต้องใส่ตำแหน่งและข้อมูลที่จะแก้ไข ");
+            }
+        }
+        private int Index()
+        {
+            int index;
+            if (int.TryParse(GetIndextextBox.Text, out index))
+            {
+                return --index;
+            }
+            else
+            {
+                return -2;
+            }
+        }
+
+        private void ShowAll_Click(object sender, EventArgs e)
+        {
+            NameAllListBox.Items.Clear();
+            for (int i = 0; i < C.size(); i++) 
+            {
+                NameAllListBox.Items.Add(C.get(i));
             }
         }
     }
